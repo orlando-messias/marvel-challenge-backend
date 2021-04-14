8b28e304585501f771b776cfe6a5a5796343457a
orlando-messias <programadorlando@gmail.com>
2021-04-14 20:36:18 -0300

    updates db config

--- src/config/db.ts
+++ src/config/db.ts
@@ -3,8 +3,4 @@ import { createConnection } from 'typeorm';
 export const connection = async () => {
   const conn = await createConnection();
   console.log(`Connected to db ${conn.options.database}`);
-  
-  process.on('SIGINT', () => {
-    conn.close().then(() => console.log('bd marvel done'));
-  });
 };
