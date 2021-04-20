af28ccca37137355f5b01bcc434ddcca919734ce
orlando-messias <programadorlando@gmail.com>
2021-04-20 17:51:39 -0300

    local config

--- src/config/db.ts
+++ src/config/db.ts
@@ -5,14 +5,7 @@ dotenv.config();
 import { createConnection } from 'typeorm';
 
 export const connection = async () => {
-  const conn = await createConnection({
-    type: "mysql",
-    host: process.env.HOST_DB,
-    port: 3306,
-    username: process.env.USER,
-    password: process.env.PASS,
-    database: process.env.DATABASE
-  });
+  const conn = await createConnection();
 
   console.log(`Connected to db ${conn.options.database}`);
 };
