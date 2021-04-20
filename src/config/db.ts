f9c0f0ec9886711d9325d6d5e661b063f9085c94
orlando-messias <programadorlando@gmail.com>
2021-04-20 17:19:33 -0300

    db env variables

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,14 +1,18 @@
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
 import { createConnection } from 'typeorm';
 
 export const connection = async () => {
   const conn = await createConnection({
     type: "mysql",
-    host: "bhcsonbdv3x3fgosus1l-mysql.services.clever-cloud.com",
+    host: process.env.HOST_DB,
     port: 3306,
-    username: "uw74u9fhfzbu5plv",
-    password: "871EAG1yM7Ord9FrB4tM",
-    database: "bhcsonbdv3x3fgosus1l"
+    username: process.env.USER,
+    password: process.env.PASS,
+    database: process.env.DATABASE
   });
-  
+
   console.log(`Connected to db ${conn.options.database}`);
 };
