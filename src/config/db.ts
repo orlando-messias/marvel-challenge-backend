e4cf5b170b8769a7c9f35389d4c78107e60c169c
orlando-messias <programadorlando@gmail.com>
2021-04-20 16:53:02 -0300

    new db configuration

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,6 +1,14 @@
 import { createConnection } from 'typeorm';
 
 export const connection = async () => {
-  const conn = await createConnection();
+  const conn = await createConnection({
+    type: "mysql",
+    host: "bhcsonbdv3x3fgosus1l-mysql.services.clever-cloud.com",
+    port: 3306,
+    username: "uw74u9fhfzbu5plv",
+    password: "871EAG1yM7Ord9FrB4tM",
+    database: "bhcsonbdv3x3fgosus1l"
+  });
+  
   console.log(`Connected to db ${conn.options.database}`);
 };
